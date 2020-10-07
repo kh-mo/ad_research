e51d43360379524bc462ba378b1d5f9bf48bf368
kh-mo <momkdh2@naver.com>
2020-10-08 00:13:26 +0900

    add tensorboard function

--- ae.py
+++ ae.py
@@ -9,55 +9,57 @@ from utils import save_model, convert_multimodal_mnist
 
 import torch
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 if __name__ == "__main__":
     # config
     except_class = 1
     batch_size = 256
+    epochs = 100
 
     # data
     trainset, testset = get_dataset("data")
     trainset = convert_multimodal_mnist(trainset, except_class=except_class)
     train_loader = DataLoader(trainset, batch_size=batch_size)
     test_loader = DataLoader(testset, batch_size=batch_size)
+    print("Data Load Done.")
 
     # model
     encoder = Encoder(input_size=28*28*1, output_size=100, n_layers=3)
     decoder = Encoder(input_size=100, output_size=28*28*1, n_layers=3)
     model = AutoEncoder(encoder, decoder).to("cuda")
+    print("Model Load Done.")
 
     # loss & optimizer
     mse = torch.nn.MSELoss()
     opt = torch.optim.SGD(model.parameters(), lr=0.01)
 
     # training
-    epochs = 10
-    training_loss = []
-    trained_loss = []
-    for img, label in train_loader:
-        flat_img = torch.flatten(img, start_dim=1).to("cuda")
-        pred = model(flat_img)
-        loss = mse(flat_img, pred)
-        training_loss.append(loss)
-
-    for idx, epoch in enumerate(range(epochs)):
-        for img, label in train_loader:
+    max_i_idx = 0
+    writer = SummaryWriter("runs/ae_excls1")
+    for e_idx, epoch in enumerate(range(epochs)):
+        epoch_loss = []
+        for i_idx, (img, label) in enumerate(train_loader):
             opt.zero_grad()
+
             flat_img = torch.flatten(img, start_dim=1).to("cuda")
             pred = model(flat_img)
             loss = mse(flat_img, pred)
+            epoch_loss.append(loss)
+
+            if max_i_idx < i_idx:
+                max_i_idx = i_idx
+            writer.add_scalar('iteration loss', loss, i_idx + (e_idx * max_i_idx))
+
             loss.backward()
             opt.step()
+        epoch_loss = sum(epoch_loss) / len(epoch_loss)
+        writer.add_scalar('epoch loss', epoch_loss, e_idx)
+    writer.close()
+    print("Training Done.")
 
-    for img, label in train_loader:
-        flat_img = torch.flatten(img, start_dim=1).to("cuda")
-        pred = model(flat_img)
-        loss = mse(flat_img, pred)
-        trained_loss.append(loss)
-
-    print("학습 전 : {}".format(sum(training_loss) / len(training_loss)))
-    print("학습 후 : {}".format(sum(trained_loss) / len(trained_loss)))
-
+    # saving
     path = os.path.join(os.getcwd(), "save_model")
     name = "ae"
     save_model(model, path, name)
+    print("Saving Done.")
