ed9755a3fc43be2fa7f24e4bb85fb849c358d55a
kh-mo <momkdh2@naver.com>
2020-10-09 23:46:44 +0900

    add metric, test, online image

--- ae.py
+++ ae.py
@@ -5,61 +5,104 @@ sys.path.append(os.path.join(os.getcwd(), "datasets"))
 
 from anomalydetection.autoencoder import AutoEncoder, Encoder
 from datasets.mnist import get_dataset
-from utils import save_model, convert_multimodal_mnist
+from utils import save_model, convert_multimodal_mnist, show_online_image
 
+import numpy as np
+from sklearn.metrics import roc_auc_score
 import torch
+import torchvision
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 if __name__ == "__main__":
-    # config
-    except_class = 1
-    batch_size = 256
-    epochs = 100
+    # 모든 class에 대해 실험
+    for except_class in list(range(10)):
+        # config
+        # except_class = 1
+        batch_size = 256
+        epochs = 100
+        bottleneck_size = 100
+        n_layers = 5
+        learning_rate = 0.001
+        config_summary_name = "excls"+str(except_class) + \
+                              "_nlayers"+str(n_layers)
 
-    # data
-    trainset, testset = get_dataset("data")
-    trainset = convert_multimodal_mnist(trainset, except_class=except_class)
-    train_loader = DataLoader(trainset, batch_size=batch_size)
-    test_loader = DataLoader(testset, batch_size=batch_size)
-    print("Data Load Done.")
+        # data
+        trainset, testset = get_dataset("data")
+        trainset = convert_multimodal_mnist(trainset, except_class=except_class)
+        showing_train_loader = DataLoader(show_online_image(trainset, except_class=except_class, amount=10), batch_size=90)
+        showing_test_loader = DataLoader(show_online_image(testset, except_class=None, amount=10), batch_size=100)
+        train_loader = DataLoader(trainset, batch_size=batch_size)
+        test_loader = DataLoader(testset, batch_size=batch_size)
+        print("Data Load Done.")
 
-    # model
-    encoder = Encoder(input_size=28*28*1, output_size=100, n_layers=3)
-    decoder = Encoder(input_size=100, output_size=28*28*1, n_layers=3)
-    model = AutoEncoder(encoder, decoder).to("cuda")
-    print("Model Load Done.")
+        # model
+        encoder = Encoder(input_size=28*28*1, output_size=bottleneck_size, n_layers=n_layers)
+        decoder = Encoder(input_size=bottleneck_size, output_size=28*28*1, n_layers=n_layers)
+        model = AutoEncoder(encoder, decoder).to("cuda")
+        print("Model Load Done.")
 
-    # loss & optimizer
-    mse = torch.nn.MSELoss()
-    opt = torch.optim.SGD(model.parameters(), lr=0.01)
+        # loss & optimizer
+        mse = torch.nn.MSELoss()
+        opt = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    # training
-    max_i_idx = 0
-    writer = SummaryWriter("runs/ae_excls1")
-    for e_idx, epoch in enumerate(range(epochs)):
-        epoch_loss = []
-        for i_idx, (img, label) in enumerate(train_loader):
-            opt.zero_grad()
+        # training
+        max_i_idx = 0
+        writer = SummaryWriter("runs/ae/"+config_summary_name)
+        for e_idx, epoch in enumerate(range(epochs)):
+            epoch_loss = []
+            for i_idx, (img, label) in enumerate(train_loader):
+                opt.zero_grad()
 
+                flat_img = torch.flatten(img, start_dim=1).to("cuda")
+                pred = model(flat_img)
+                loss = mse(flat_img, pred)
+                epoch_loss.append(loss)
+
+                if max_i_idx < i_idx:
+                    max_i_idx = i_idx
+                writer.add_scalar('iteration loss', loss, i_idx + (e_idx * max_i_idx))
+
+                loss.backward()
+                opt.step()
+
+            # 중간 이미지 보기
+            if (e_idx+1) % 10 == 0:
+                for s_idx, (img, label) in enumerate(showing_train_loader):
+                    flat_img = torch.flatten(img, start_dim=1).to("cuda")
+                    pred = model(flat_img)
+                    grid = torchvision.utils.make_grid(pred.reshape(-1,1,28,28).detach().to("cpu"), nrow=10)
+                    writer.add_image("epochs"+str(e_idx+1)+"_train_images", grid, 0)
+
+            epoch_loss = sum(epoch_loss) / len(epoch_loss)
+            writer.add_scalar('epoch loss', epoch_loss, e_idx)
+
+        print("Training Done.")
+
+        # saving
+        path = os.path.join(os.getcwd(), "save_model")
+        name = "ae_" + config_summary_name
+        save_model(model, path, name)
+        print("Saving Done.")
+
+        # test 이미지 보기
+        for img, label in showing_test_loader:
             flat_img = torch.flatten(img, start_dim=1).to("cuda")
             pred = model(flat_img)
-            loss = mse(flat_img, pred)
-            epoch_loss.append(loss)
+            grid = torchvision.utils.make_grid(pred.reshape(-1, 1, 28, 28).detach().to("cpu"), nrow=10)
+            writer.add_image("test_images", grid, 0)
 
-            if max_i_idx < i_idx:
-                max_i_idx = i_idx
-            writer.add_scalar('iteration loss', loss, i_idx + (e_idx * max_i_idx))
+        writer.close()
 
-            loss.backward()
-            opt.step()
-        epoch_loss = sum(epoch_loss) / len(epoch_loss)
-        writer.add_scalar('epoch loss', epoch_loss, e_idx)
-    writer.close()
-    print("Training Done.")
+        # testing
+        score = []
+        y_true = []
+        for img, label in test_loader:
+            flat_img = torch.flatten(img, start_dim=1).to("cuda")
+            pred = model(flat_img)
 
-    # saving
-    path = os.path.join(os.getcwd(), "save_model")
-    name = "ae"
-    save_model(model, path, name)
-    print("Saving Done.")
+            score += ((flat_img-pred)**2).sum(axis=1).detach().to("cpu").tolist()
+            y_true += np.where(label==except_class, 1, 0).tolist()
+
+        print("auroc : {}".format(roc_auc_score(y_true, score)))
+
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import copy
 import torch
 import numpy as np
 
@@ -6,13 +7,33 @@ def save_model(model, path, name):
     os.makedirs(path, exist_ok=True)
     torch.save(model, os.path.join(path, name))
 
-def convert_multimodal_mnist(dataset, except_class):
+def convert_multimodal_mnist(dset, except_class):
     '''
-    mnist 10개 클래스 중 특정 클래스 데이터를 제외
+        mnist 10개 클래스 중 특정 클래스 데이터를 제외
+        단, 테스트때는 모든 class 사용
     '''
-    total_class = [i for i in range(10) if i != except_class]
+    dataset = copy.deepcopy(dset)
+    if except_class is None:
+        total_class = [i for i in range(10)]
+    else:
+        total_class = [i for i in range(10) if i != except_class]
 
     normal_cls_idx = np.where(np.isin(dataset.targets, total_class))
     dataset.data = dataset.data[normal_cls_idx[0]]
     dataset.targets = dataset.targets[normal_cls_idx[0]]
+    return dataset
+
+def show_online_image(dset, except_class, amount):
+    '''
+        중간중간 생성되는 이미지를 보여주기 위한 데이터셋
+    '''
+    dataset = copy.deepcopy(dset)
+    total_class = [i for i in range(10) if i != except_class]
+
+    online_img_idx = []
+    for i in total_class:
+        online_img_idx += np.where(np.isin(dataset.targets, i))[0][:amount].tolist()
+
+    dataset.data = dataset.data[online_img_idx]
+    dataset.targets = dataset.targets[online_img_idx]
     return dataset
